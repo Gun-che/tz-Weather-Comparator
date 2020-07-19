b964a232c52382936f7dae2aec66226fd8dfa772
Ivan <goncharov1995.2@gmail.com>
2020-07-19 19:16:14 +0300

    create home page and README.md

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,8 +3,28 @@ import s from './index.module.scss'
 
 export const Home: React.FC = () => {
   return (
-    <section className={s.wrap}>
-      Home
+    <section className={s.wrap + ' containerCust'}>
+      <h2>
+        Weather Comparator
+      </h2>
+      <ul>
+        <h3>Cтек технологий, используемых для разработки:</h3>
+        <li><i className="material-icons">flash_on</i>TypeScript</li>
+        <li><i className="material-icons">flash_on</i>React, Redux, Redux-saga, React-router</li>
+        <li><i className="material-icons">flash_on</i>CSS-библиотека Materialize</li>
+      </ul>
+      <p>
+        Использован <a href="https://openweathermap.org/api">OpenWeather API</a>
+      </p>
+      <ul>
+        <h3>
+          На странице вы можете:
+        </h3>
+        <li><i className="material-icons">flash_on</i>Узнать текущую погоду</li>
+        <li><i className="material-icons">flash_on</i>Посмотреть почасовой прогноз на 2 дня</li>
+        <li><i className="material-icons">flash_on</i>Посмотреть посуточный прогнеоз на неделю</li>
+        <li><i className="material-icons">flash_on</i>Сравнить погоду между 2 и более локациями во вкладке "Weather Comparator"</li>
+      </ul>
     </section>
   )
 }
